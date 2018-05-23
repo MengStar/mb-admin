2fde99ad123349506773498239f736dce21d2ed8
MengStar <641510128@qq.com>
2018-05-23 15:42:19 +0800

    请求新增 头部字段 Authorization

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import fetch from 'dva/fetch';
 import { notification } from 'antd';
 import { routerRedux } from 'dva/router';
 import store from '../index';
+import { getToken } from './authority';
 
 const codeMessage = {
   200: '服务器成功返回请求的数据。',
@@ -47,6 +48,10 @@ export default function request(url, options) {
     credentials: 'include',
   };
   const newOptions = { ...defaultOptions, ...options };
+  newOptions.headers = {
+    Authorization: getToken(),
+    ...newOptions.headers,
+  };
   if (newOptions.method === 'POST' || newOptions.method === 'PUT') {
     if (!(newOptions.body instanceof FormData)) {
       newOptions.headers = {
